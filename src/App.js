24bca890797faba180f9b8fdd6d3a9ca82543379
khushi67890 <khushiprajapati3005@gmail.com>
2023-08-15 15:36:44 +0530

    Completed sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
+import ListUserComponent from './components/ListUserComponent';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import HeaderComponent from './components/HeaderComponent';
+import CreateUserComponent from './components/CreateUserComponent';
+import ViewUserComponent from './components/ViewUserComponent'
+import UpdateUserComponent from './components/UpdateUserComponent'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <HeaderComponent/>
+        <div className='container'>
+          <Switch>
+            <Route path="/" exact component={ListUserComponent}></Route>
+            <Route path="/users" exact component={ListUserComponent}></Route>
+            <Route component={CreateUserComponent} path="/add-user/:id"></Route>
+            <Route component={UpdateUserComponent} path="/update-user/:id">
+
+            </Route>
+            <Route component={ViewUserComponent} path="/view-user:id">
+              
+            </Route>
+            
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
